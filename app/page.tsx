c5811ff859e91246e9af942249c6e1662ded31f4
ZBM Zubayer <zbmzubayer@gmail.com>
2023-11-09 01:18:35 +0600

    RootLayout metadata and font setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="h1-bold">Stack overflow clone - Dev Overflow</h1>
+      <h1 className="h1-bold">
+        Stack overflow clone - <span className="font-spaceGrotesk">Dev Overflow</span>
+      </h1>
     </main>
   );
 }
