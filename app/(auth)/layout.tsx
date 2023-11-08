9e70b9889463736b4e9e00a31dd8f0f124d6d948
ZBM Zubayer <zbmzubayer@gmail.com>
2023-11-09 02:17:09 +0600

    Clerk authentication

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,3 @@
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return <main className="flex min-h-screen items-center justify-center">{children}</main>;
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter, Space_Grotesk } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
@@ -18,8 +19,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} ${spaceGrotesk.variable}`}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
+        },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className} ${spaceGrotesk.variable}`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
