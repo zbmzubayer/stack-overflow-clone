f5cd0ce0caa13d36297270aac2af46c5e37c8aa6
ZBM Zubayer <zbmzubayer@gmail.com>
2023-11-17 16:42:18 +0600

    Add theme-toggle component and theme-provider context

---
+++ app/(root)/_components/theme-toggle.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import { LaptopIcon, MoonIcon, SunIcon } from 'lucide-react';
+import { useTheme } from 'next-themes';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="focus-visible:ring-0 focus-visible:ring-offset-0"
+        >
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 fill-orange-500 text-orange-500 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:fill-orange-500 dark:text-orange-500" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem
+          onClick={() => setTheme('light')}
+          className={`${theme === 'light' ? 'text-orange-500' : ''}`}
+        >
+          <SunIcon className="mr-2 h-4 w-4" />
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => setTheme('dark')}
+          className={`${theme === 'dark' ? 'text-orange-500' : ''}`}
+        >
+          <MoonIcon className="mr-2 h-4 w-4" />
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => setTheme('system')}
+          className={`${theme === 'system' ? 'text-orange-500' : ''}`}
+        >
+          <LaptopIcon className="mr-2 h-4 w-4" />
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
