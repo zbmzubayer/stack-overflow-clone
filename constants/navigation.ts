e99abdda80721cfbe4a2bcaf50e36b1c4ba8b785
ZBM Zubayer <zbmzubayer@gmail.com>
2023-11-17 16:50:21 +0600

    Add new constants for filters, navigation, and no result messages

---
+++ constants/navigation.ts
@@ -0,0 +1,43 @@
+type SidebarLink = {
+  imgURL: string;
+  route: string;
+  label: string;
+};
+
+export const sidebarLinks: SidebarLink[] = [
+  {
+    imgURL: '/assets/icons/home.svg',
+    route: '/',
+    label: 'Home',
+  },
+  {
+    imgURL: '/assets/icons/users.svg',
+    route: '/community',
+    label: 'Community',
+  },
+  {
+    imgURL: '/assets/icons/star.svg',
+    route: '/collection',
+    label: 'Collections',
+  },
+  {
+    imgURL: '/assets/icons/suitcase.svg',
+    route: '/jobs',
+    label: 'Find Jobs',
+  },
+  {
+    imgURL: '/assets/icons/tag.svg',
+    route: '/tags',
+    label: 'Tags',
+  },
+  {
+    imgURL: '/assets/icons/user.svg',
+    route: '/profile',
+    label: 'Profile',
+  },
+  {
+    imgURL: '/assets/icons/question.svg',
+    route: '/ask-question',
+    label: 'Ask a question',
+  },
+];
---
+++ constants/no-result.ts
@@ -0,0 +1,7 @@
+export const questionNoResult = {
+  title: 'There is no question to show',
+  description:
+    'Be the first to break the silence. 🚀 Ask question and kickstart the discussion. Your query could be the next big thing that others learn from. Get involved! 💡',
+  buttonText: 'Ask a question',
+  buttonLink: '/ask-question',
+};
