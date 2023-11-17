d35c3d18b95653d5d624a212c23b205b568c2869
ZBM Zubayer <zbmzubayer@gmail.com>
2023-11-17 20:12:57 +0600

    Clerk webhook secret added

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,12 +1,13 @@
-import { Webhook } from 'svix';
-import { headers } from 'next/headers';
-import { WebhookEvent } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
+import { headers } from 'next/headers';
+import { Webhook } from 'svix';
+import { WebhookEvent } from '@clerk/nextjs/server';
 import { createUser, deleteUser, updateUser } from '@/actions/user.action';
+import envConfig from '@/config';
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = envConfig.CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,7 @@
 const envConfig = {
   MONGODB_URI: process.env.MONGODB_URI as string,
   TINY_API_KEY: process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY,
+  CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET,
 };
 
 export default envConfig;
