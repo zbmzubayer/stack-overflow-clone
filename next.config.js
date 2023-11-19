ea4c5628a6a9788256406088db523a165e95d5df
ZBM Zubayer <zbmzubayer@gmail.com>
2023-11-20 00:26:05 +0600

    Add remotePatterns for images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { hostname } = require('os');
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+      {
+        protocol: 'http',
+        hostname: '*',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
