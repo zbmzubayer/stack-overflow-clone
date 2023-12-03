a0f91494d4c9f65e14ce8c355750b5848a786398
ZBM Zubayer <zbmzubayer@gmail.com>
2023-12-03 23:53:13 +0600

    Refactor code - simplified expression using OR

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -57,7 +57,7 @@ export default async function Home({ searchParams }: SearchParamsProps) {
           />
         )}
       </div>
-      <Pagination pageNumber={searchParams?.page ? +searchParams.page : 1} isNext={isNext} />
+      <Pagination pageNumber={Number(searchParams.page) || 1} isNext={isNext} />
     </>
   );
 }
