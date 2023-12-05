e21709d8232478f7e8dff77d5689654d60d756f9
ZBM Zubayer <zbmzubayer@gmail.com>
2023-12-06 04:29:41 +0600

    refactor: middleware public routes updated

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,15 @@ import { authMiddleware } from '@clerk/nextjs';
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhook', '/question/:id', '/tags', '/tag/:id', '/community', '/jobs'],
+  publicRoutes: [
+    '/',
+    '/api/webhook',
+    '/question/:id',
+    '/tags',
+    '/tags/:id',
+    '/community',
+    '/profile/:id',
+  ],
   ignoredRoutes: ['/api/webhook'],
 });
 
