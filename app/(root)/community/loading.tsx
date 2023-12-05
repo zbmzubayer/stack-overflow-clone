5996b8ff4e18816845ca8e0060f40c6f7b6b9a7a
ZBM Zubayer <zbmzubayer@gmail.com>
2023-12-06 04:17:18 +0600

    fix: Community page loading skeleton

--- app/(root)/community/loading.tsx
+++ app/(root)/community/loading.tsx
@@ -10,7 +10,7 @@ export default function Loading() {
         <Skeleton className="mt-10 h-10 flex-1" />
         <Skeleton className="mt-10 h-10 w-44" />
       </div>
-      <div className="mt-12 grid grid-cols-2 gap-4 lg:grid-cols-3">
+      <div className="mt-12 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 min-[1700px]:grid-cols-4 min-[2200px]:grid-cols-5">
         {users.map((user) => (
           <Skeleton key={user} className="h-[260px] rounded-2xl" />
         ))}
